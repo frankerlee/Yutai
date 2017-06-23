85f4cc105a2264f9ec29e02fb137d4dc2ead44d1
EthanWang <ethan980301>
2017-06-23 15:54:21 +0800

    增加IPipelineType

---
+++ Yutai.Pipeline.Config/interfaces/IPipelineType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yutai.Pipeline.Config.Interfaces
+{
+    public interface IPipelineType
+    {
+        string Code { get; set; }
+        string Name { get; set; }
+        string AutoNames { get; set; }
+    }
+
+    public enum enumPipelineDataType
+    {
+        Point,
+        Line,
+        ThreeD,
+        AssPoint,
+        AssLine,
+        AnnoPoint,
+        AnnoLine,
+        Annotation,
+        Other
+    }
+}
