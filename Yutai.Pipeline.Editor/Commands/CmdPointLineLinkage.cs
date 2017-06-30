4229a11724902611bdb365b5ed60e49b78a9510d
konglingjie <547518762@qq.com>
2017-06-30 17:07:56 +0800

    整理部分管线编辑工具

---
+++ Yutai.Pipeline.Editor/Commands/CmdPointLineLinkage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yutai.Pipeline.Config.Interfaces;
+using Yutai.Plugins.Concrete;
+using Yutai.Plugins.Enums;
+using Yutai.Plugins.Interfaces;
+
+namespace Yutai.Pipeline.Editor.Commands
+{
+    class CmdPointLineLinkage : YutaiTool
+    {
+        private PipelineEditorPlugin _plugin;
+        private IPipelineConfig _config;
+
+        public CmdPointLineLinkage(IAppContext context, PipelineEditorPlugin plugin)
+        {
+            OnCreate(context);
+            _plugin = plugin;
+        }
+
+        public override void OnClick(object sender, EventArgs args)
+        {
+            OnClick();
+        }
+
+        public sealed override void OnCreate(object hook)
+        {
+            _context = hook as IAppContext;
+            base.m_caption = "点线联动";
+            base.m_category = "PipelineEditor";
+            //base.m_bitmap = Properties.Resources.icon_valve;
+            base.m_name = "PipelineEditor_PointLineLinkage";
+            base._key = "PipelineEditor_PointLineLinkage";
+            base.m_toolTip = "点线联动";
+            base.m_checked = false;
+            base.m_message = "点线联动";
+            base.m_enabled = true;
+            base._itemType = RibbonItemType.Tool;
+        }
+
+        public override void OnClick()
+        {
+            _context.SetCurrentTool(this);
+        }
+
+        public override void OnDblClick()
+        {
+
+        }
+
+        public override void OnMouseDown(int button, int shift, int x, int y)
+        {
+        }
+    }
+}
